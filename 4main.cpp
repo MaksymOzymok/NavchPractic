5097944de8c3ec1400b81b9416db736d868cbdb2
OzymokMax <Ozymokmax1@ukr.net>
2019-03-04 15:18:14 +0200

    Програма для знаходження радіусів

---
+++ 4main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+ using namespace std; 
+ int main() {         float a,r;        
+ for(a = 1;a<=5;a+=0.25)
+{
+  r = (a*1.4)/3;   
+   cout << a <<" : "<< r <<endl; 
+      } 
+}
